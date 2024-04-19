389c7b923878f8f0ac8ac17d40efcc928a805c64
verraputri <verraantonputri@gmail.com>
2024-04-19 16:29:31 +0700

    update tugas

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:tugas_verra/providers/app_state.dart';
+import 'package:tugas_verra/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MainApp());
+}
 
-class MyApp extends StatelessWidget {
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('staxt')),
-        body: Center(
-          child: Stack(
-            children: <Widget>[
-              Align(
-                alignment: Alignment.center,
-                child: Container(
-                  color: Colors.red,
-                  height: 100,
-                  width: 100,
-                ), // Container
-              ),
-              Align(
-                // Align
-                alignment: Alignment.center,
-                child: Container(
-                  color: Colors.blue,
-                  height: 100,
-                  width: 80,
-                  margin: EdgeInsets.only(top: 20),
-                ), // Container
-              ),
-              Align(
-                // Align
-                alignment: Alignment.center,
-                child: Container(
-                  color: Colors.purple,
-                  height: 70,
-                  width: 70,
-                  margin: EdgeInsets.only(top: 20),
-                ), // Container
-              ), // Align
-            ], // <Widget>[]
-          ), // Stack
-        ), // Center
-      ), // Scaffold // MaterialApp
+    return ChangeNotifierProvider(
+      create: (context) => AppState(),
+      child: MaterialApp(
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        title: "Namer App",
+        home: const HomeScreen(),
+      ),
     );
   }
 }
